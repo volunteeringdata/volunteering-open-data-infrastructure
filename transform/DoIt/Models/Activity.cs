f5afea4007ddf7137a06228c8808166e9a1679a8
Samu Lang <langsamu@hotmail.com>
2025-10-29 14:00:00 +0000

    Serialization improvements

--- transform/DoIt/Models/Activity.cs
+++ transform/DoIt/Models/Activity.cs
@@ -2,87 +2,102 @@ namespace DoIt.Models;
 
 public class Activity
 {
+    #region Calculated convenience properties
+
     [JsonIgnore]
     public string Id => IdObject.Value;
-
-    [JsonIgnore]
-    public DateTimeOffset CreatedAt => CreatedAtObject.Value;
-
-    [JsonIgnore]
-    public DateTimeOffset UpdatedAt => UpdatedAtObject.Value;
-
-    [JsonIgnore]
-    public string Ecosystem => EcosystemObject.Value;
-
-    [JsonIgnore]
-    public string Organization => OrganizationObject.Value;
 
     [JsonIgnore]
     public string App => AppObject.Value;
 
     [JsonIgnore]
-    public IEnumerable<string> PublishedApps => PublishedAppObjects.Select(a => a.Value);
+    public DateTimeOffset CreatedAt => CreatedAtObject.Value;
 
     [JsonIgnore]
-    public string ActivityDefinition => ActivityDefinitionObject.Value;
+    public DateTimeOffset? DueDate => DueDateObject?.Value;
+
+    [JsonIgnore]
+    public string Ecosystem => EcosystemObject.Value;
+
+    [JsonIgnore]
+    public DateTimeOffset? EndDate => EndDateObject?.Value;
 
     [JsonIgnore]
     public Uri? MeetingLink => string.IsNullOrWhiteSpace(MeetingLinkString) ? null : new Uri(MeetingLinkString);
 
     [JsonIgnore]
+    public string Organization => OrganizationObject.Value;
+
+    [JsonIgnore]
+    public IEnumerable<string> PublishedApps => PublishedAppObjects.Select(a => a.Value);
+
+    [JsonIgnore]
     public IEnumerable<Region> Regions => RegionsNullable ?? [];
+
+    [JsonIgnore]
+    public DateTimeOffset? StartDate => StartDateObject?.Value;
+
+    [JsonIgnore]
+    public DateTimeOffset UpdatedAt => UpdatedAtObject.Value;
+
+    #endregion
+
+    // __v (seems irrelevant, probably version tracking, some null, some 0, some 1)
 
     [JsonPropertyName("_id")]
     public required Id IdObject { get; set; }
 
-    [JsonPropertyName("volunteerNumber")]
-    public int? VolunteerNumber { get; set; }
-
-    [JsonPropertyName("isVolunteerNumberLimited")]
-    public bool? IsVolunteerNumberLimited { get; set; }
-
-    [JsonPropertyName("isOnline")]
-    public bool? IsOnline { get; set; }
-
-    [JsonPropertyName("meetingLink")]
-    public string? MeetingLinkString { get; set; }
-
-    [JsonPropertyName("externalApplyLink")]
-    public Uri? ExternalApplyLink { get; set; }
-
-    [JsonPropertyName("address")]
-    public Address? Address { get; set; }
+    // activityDefinition (redundant with subdocument)
 
     [JsonPropertyName("activityDefinitionSubDocument")]
     public required ActivityDefinitionSubDocument ActivityDefinitionSubDocument { get; set; }
 
-    [JsonPropertyName("bookingsNumber")]
-    public required int BookingsNumber { get; set; }
+    [JsonPropertyName("address")]
+    public Address? Address { get; set; }
+
+    [JsonPropertyName("app")]
+    public required Id AppObject { get; set; }
 
     // All zeros except 1*1+1*2
     [JsonPropertyName("attendeesNumber")]
     public required int AttendeesNumber { get; set; }
 
-    // All zeros except a few nulls
-    [JsonPropertyName("teamsNumber")]
-    public int? TeamsNumber { get; set; }
+    [JsonPropertyName("bookingsNumber")]
+    public required int BookingsNumber { get; set; }
 
-    // All zeros except a few nulls
-    [JsonPropertyName("teamsMinSize")]
-    public int? TeamsMinSize { get; set; }
+    [JsonPropertyName("createdAt")]
+    public required Date CreatedAtObject { get; set; }
 
-    // All zeros except a few nulls
-    [JsonPropertyName("teamsMaxSize")]
-    public int? TeamsMaxSize { get; set; }
+    [JsonPropertyName("deleted")]
+    public bool? Deleted { get; set; }
+
+    [JsonPropertyName("dueDate")]
+    public Date? DueDateObject { get; set; }
 
     [JsonPropertyName("ecosystem")]
     public required Id EcosystemObject { get; set; }
 
+    [JsonPropertyName("endDate")]
+    public Date? EndDateObject { get; set; }
+
+    [JsonPropertyName("externalApplyLink")]
+    public Uri? ExternalApplyLink { get; set; }
+
+    // externalId (all null) TODO: Check
+
+    // externalProvider (all null) TODO: Check
+
+    [JsonPropertyName("isOnline")]
+    public bool? IsOnline { get; set; }
+
+    [JsonPropertyName("isVolunteerNumberLimited")]
+    public bool? IsVolunteerNumberLimited { get; set; }
+
+    [JsonPropertyName("meetingLink")]
+    public string? MeetingLinkString { get; set; }
+
     [JsonPropertyName("organization")]
     public required Id OrganizationObject { get; set; }
-
-    [JsonPropertyName("app")]
-    public required Id AppObject { get; set; }
 
     [JsonPropertyName("publishedApps")]
     public required IEnumerable<Id> PublishedAppObjects { get; set; }
@@ -90,15 +105,24 @@ public class Activity
     [JsonPropertyName("regions")]
     public IEnumerable<Region>? RegionsNullable { get; set; }
 
-    [JsonPropertyName("activityDefinition")]
-    public required Id ActivityDefinitionObject { get; set; }
+    [JsonPropertyName("startDate")]
+    public Date? StartDateObject { get; set; }
 
-    [JsonPropertyName("deleted")]
-    public bool? Deleted { get; set; }
+    // All zeros except a few nulls
+    [JsonPropertyName("teamsMaxSize")]
+    public int? TeamsMaxSize { get; set; }
 
-    [JsonPropertyName("createdAt")]
-    public required Date CreatedAtObject { get; set; }
+    // All zeros except a few nulls
+    [JsonPropertyName("teamsMinSize")]
+    public int? TeamsMinSize { get; set; }
+
+    // All zeros except a few nulls
+    [JsonPropertyName("teamsNumber")]
+    public int? TeamsNumber { get; set; }
 
     [JsonPropertyName("updatedAt")]
     public required Date UpdatedAtObject { get; set; }
+
+    [JsonPropertyName("volunteerNumber")]
+    public int? VolunteerNumber { get; set; }
 }
