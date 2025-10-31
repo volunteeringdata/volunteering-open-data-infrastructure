33f742d0b5b0ec3e70cb3aa11554b6a862221634
Samu Lang <langsamu@hotmail.com>
2025-10-31 16:12:08 +0000

    Query endpoint working.
    Resources, framing, SPARQL connector.

---
+++ Query/Controllers/DefaultController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using VDS.RDF;
+using VDS.RDF.JsonLd;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+using VDS.RDF.Writing;
+
+namespace Query.Controllers;
+
+[Route("/{name}")]
+[ApiController]
+public class DefaultController(HttpClient httpClient) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get(string name, CancellationToken ct)
+    {
+        var resourceName = $"Query.Endpoints.{name}.query.sparql";
+        using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+        if (stream is null)
+        {
+            return this.NotFound();
+        }
+
+        using var reader = new StreamReader(stream);
+        var sparqlText = reader.ReadToEnd();
+
+        var sparqlQuery = new SparqlQueryParser().ParseFromString(sparqlText);
+        var sparqlClient = new SparqlQueryClient(httpClient, new Uri("https://202510300952apptriplestoretest.azurewebsites.net/sparql"));
+
+        if (sparqlQuery.QueryType == SparqlQueryType.Construct || sparqlQuery.QueryType == SparqlQueryType.Describe || sparqlQuery.QueryType == SparqlQueryType.DescribeAll)
+        {
+            var result = await sparqlClient.QueryWithResultGraphAsync(sparqlText, ct);
+
+            var frameResourceName = $"Query.Endpoints.{name}.frame.jsonld";
+            using var frameStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(frameResourceName);
+            if (frameStream is null)
+            {
+                throw new Exception("Frame resource not found");
+            }
+
+            using var frameReader = new StreamReader(frameStream);
+            var frameText = frameReader.ReadToEnd();
+
+            var frame = JToken.Parse(frameText);
+
+            var ts = new TripleStore();
+            ts.Add(result);
+            var jsonLdNode = new JsonLdWriter().SerializeStore(ts);
+            var framingResult = JsonLdProcessor
+                .Frame(jsonLdNode, frame, new JsonLdProcessorOptions())
+                .ToString();
+
+            return this.Ok(framingResult);
+        }
+        else
+        {
+            var resultSetResult = await sparqlClient.QueryWithResultSetAsync(sparqlText, ct);
+            var resultText = VDS.RDF.Writing.StringWriter.Write(resultSetResult, new SparqlJsonWriter());
+            return this.Ok(resultText);
+        }
+    }
+}
--- Query/Program.cs
+++ Query/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapControllers();
 app.Run();
