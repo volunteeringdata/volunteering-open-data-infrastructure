a6f58cf5c4d385ea2901fb52e991c05074621f5d
Samu Lang <langsamu@hotmail.com>
2025-11-01 14:27:40 +0000

    Conneg improvements

--- Query/Program.cs
+++ Query/Program.cs
@@ -4,9 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers(options =>
 {
-    options.OutputFormatters.Insert(0, new JsonLdGraphOutputFormatter());
-    options.OutputFormatters.Insert(0, new GraphOutputFormatter());
+    options.ReturnHttpNotAcceptable = true;
+
+    // So we don't get default JSON serializations of things
+    options.OutputFormatters.Clear();
+
     options.OutputFormatters.Insert(0, new SparqlResultSetFormatter());
+    options.OutputFormatters.Insert(0, new GraphOutputFormatter());
+
+    // Added last so ends up in position 0 for highest priority
+    options.OutputFormatters.Insert(0, new JsonLdGraphOutputFormatter());
 });
 
 builder.Services.AddHttpClient();
