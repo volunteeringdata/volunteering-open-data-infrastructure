9e196a4ce543c268864d469d0c58c67461acd960
Samu Lang <langsamu@hotmail.com>
2025-11-13 15:55:41 +0000

    Preemptive validation of required endpoint parameters

--- Query/Controllers/DefaultController.cs
+++ Query/Controllers/DefaultController.cs
@@ -13,6 +13,19 @@ public class DefaultController(QueryService someService) : ControllerBase
     [HttpGet]
     public async Task<IActionResult> Get(string name, CancellationToken ct)
     {
+        if (Endpoints.TryGetValue(name, out var endpoint))
+        {
+            foreach (var item in endpoint.Parameters.Select(p => p.Name).Where(n => !Request.Query.TryGetValue(n, out _)))
+            {
+                ModelState.AddModelError(item, "This query string parameter is required");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
         var query = Request.Query.ToDictionary(i => i.Key, i => i.Value.ToString());
         var result = await someService.ExecuteNamedQueryAsync(name, query, ct);
         if (result is null)
