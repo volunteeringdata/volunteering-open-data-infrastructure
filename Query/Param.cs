e1f82870b5ba71dde273a4ddac5fc11450438917
Samu Lang <langsamu@hotmail.com>
2025-11-25 21:32:52 +0000

    Actuall examples in parameters

--- Query/Param.cs
+++ Query/Param.cs
@@ -4,9 +4,12 @@ namespace Query;
 
 internal class Param(
     string name,
+    string example,
     string datatype = XmlSpecsHelper.XmlSchemaDataTypeString)
 {
     internal string Name => name;
 
     internal string Datatype => datatype;
+
+    internal string Example => example;
 }
